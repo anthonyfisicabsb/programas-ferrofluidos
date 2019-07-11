61944c49e33060936160bda8da9017a4453aeda4
Anthony Louis <anthony.physis@gmail.com>
2019-07-11 13:54:44 -0300

    Update comments for particle struct (#2)

--- Exercicios-esfera/particula.h
+++ Exercicios-esfera/particula.h
@@ -1,5 +1,14 @@
+/*
+    This file contains a struct that represents a
+    particle caracteristics during simulation.
+    
+    The particle is describe by three vector in
+    R3 space: postion, magnetic momentum and
+    magnetization.
+*/
+
 struct particula {
-    double r[3]; // posicao da partícula
-    double m[3]; // momento magnético da partícula
-    double magz[3]; // magnetização da partícula
-}; // estrututura que representa a partícula
+    double r[3]; // position
+    double m[3]; // magnetic momentum
+    double magz[3]; // magnetization
+};
